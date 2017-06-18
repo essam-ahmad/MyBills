cafd20c5974e311ce6c996e4f22ef44c86e7a732
Ebrheem <broyemr@gmail.com>
2017-06-18 17:52:29 +0300

    اخفاء دالة الفيو

--- app/src/main/java/com/example/e3/mybills/item_Adapter.java
+++ app/src/main/java/com/example/e3/mybills/item_Adapter.java
@@ -50,72 +50,72 @@ public class item_Adapter {
     col_itm_price NUMERIC
     col_ad_date text*/
 
-//    public View getView(final int position, final View convertView, ViewGroup parent) {
-//        View vi=convertView;
-//        if(convertView==null)
-//            vi = inflater.inflate(R.layout.item_layout, null);
-//            TextView code=(TextView)vi.findViewById(R.id.nameChannle);;
-//            TextView name = (TextView) vi.findViewById(R.id.discChannle);
-//            TextView cost = (TextView)vi.findViewById(R.id.linkChannle);
-//            TextView price = (TextView)vi.findViewById(R.id.number);
-//            TextView ad_date = (TextView)vi.findViewById(R.id.number);
-//
-//        code.setText(data[position].get_col_itm_code());
-//        name.setText(data[position].get_col_itm_name());
-//        cost.setText(String.valueOf(data[position].get_col_itm_cost()));
-//        price.setText(String.valueOf(data[position].get_col_itm_price()));
-//        ad_date.setText(data[position].get_col_ad_date());
-//        final View finalVi = vi;
-//        vi.setOnLongClickListener(new View.OnLongClickListener() {
-//            @Override
-//            public boolean onLongClick(View v) {
-//
-//                AlertDialog.Builder builder = new AlertDialog.Builder(finalVi.getContext());
-//                builder.setTitle(v.getResources().getString(R.string.alert));
-//                builder.setMessage(data[position].getId()+":"+data[position].getnameitem());
-//                builder.setNegativeButton("تعديل", new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        Intent i = new Intent(finalVi.getContext(),EditItem.class);
-//                        Bundle b = new Bundle();
-//                        b.putString("getname",data[position].getnameitem());
-//                        b.putString("getdata",data[position].getdate());
-//                        b.putString("getprice",data[position].getprice());
-//                        b.putInt("getid",data[position].getId());
-//                        i.putExtras(b);
-//                        finalVi.getContext().startActivity(i);
-//                    }
-//                });
-//
-//                builder.setNeutralButton("حذف", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        AlertDialog.Builder builder = new AlertDialog.Builder(finalVi.getContext());
-//                        builder.setTitle("هل أنت متأكد من حذف الصنف:"+data[position].getnameitem());
-//                        builder.setMessage("الوصف:"+data[position].getdate()+"  ");
-//                        builder.setNegativeButton("نعم",new DialogInterface.OnClickListener() {
-//                            public void onClick(DialogInterface dialog,
-//                                                int which) {
-//                                DataBase dataBase=new DataBase(finalVi.getContext());
-//                                dataBase.deleteOneItem(data[position].getId());
-//
-//
-//                            }
-//
-//                        });
-//                        builder.setNeutralButton("لا", new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialog, int which) {
-//
-//                            }
-//                        });
-//                        builder.show();
-//
-//                    }
-//                });
-//                builder.show();
-//                return true; }
-//        });
-//        return vi;
-//    }
+    public View getView(final int position, final View convertView, ViewGroup parent) {
+        View vi=convertView;
+        if(convertView==null)
+            vi = inflater.inflate(R.layout.item_layout, null);
+            TextView code=(TextView)vi.findViewById(R.id.nameChannle);;
+            TextView name = (TextView) vi.findViewById(R.id.discChannle);
+            TextView cost = (TextView)vi.findViewById(R.id.linkChannle);
+            TextView price = (TextView)vi.findViewById(R.id.number);
+            TextView ad_date = (TextView)vi.findViewById(R.id.number);
+
+        code.setText(data[position].get_col_itm_code());
+        name.setText(data[position].get_col_itm_name());
+        cost.setText(String.valueOf(data[position].get_col_itm_cost()));
+        price.setText(String.valueOf(data[position].get_col_itm_price()));
+        ad_date.setText(data[position].get_col_ad_date());
+        final View finalVi = vi;
+        vi.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(finalVi.getContext());
+                builder.setTitle(v.getResources().getString(R.string.alert));
+                builder.setMessage(data[position].getId()+":"+data[position].getnameitem());
+                builder.setNegativeButton("تعديل", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent i = new Intent(finalVi.getContext(),EditItem.class);
+                        Bundle b = new Bundle();
+                        b.putString("getname",data[position].getnameitem());
+                        b.putString("getdata",data[position].getdate());
+                        b.putString("getprice",data[position].getprice());
+                        b.putInt("getid",data[position].getId());
+                        i.putExtras(b);
+                        finalVi.getContext().startActivity(i);
+                    }
+                });
+
+                builder.setNeutralButton("حذف", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(finalVi.getContext());
+                        builder.setTitle("هل أنت متأكد من حذف الصنف:"+data[position].getnameitem());
+                        builder.setMessage("الوصف:"+data[position].getdate()+"  ");
+                        builder.setNegativeButton("نعم",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                DataBase dataBase=new DataBase(finalVi.getContext());
+                                dataBase.deleteOneItem(data[position].getId());
+
+
+                            }
+
+                        });
+                        builder.setNeutralButton("لا", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+                        builder.show();
+
+                    }
+                });
+                builder.show();
+                return true; }
+        });
+        return vi;
+    }
 }
 
