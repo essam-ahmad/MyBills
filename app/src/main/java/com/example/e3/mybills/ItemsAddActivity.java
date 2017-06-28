80e79cfd32eb8bf31381391b64746b2139a5c0b4
essam-ahmad <essam-ahmad@users.noreply.github.com>
2017-06-28 13:28:33 +0300

    اصلاح اخطاء

--- app/src/main/java/com/example/e3/mybills/ItemsAddActivity.java
+++ app/src/main/java/com/example/e3/mybills/ItemsAddActivity.java
@@ -9,54 +9,43 @@ import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class ItemsAddActivity extends AppCompatActivity {
-    EditText addItemCode, addItemName,addItemCost, addItemprice;
-Button Save,Cancel;
+    EditText addItemCode, addItemName, addItemCost, addItemprice;
+    Button Save, Cancel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items_add);
-        addItemCode   =(EditText)findViewById(R.id.itm_code);
-        addItemName  =(EditText)findViewById(R.id.itm_name);
-        addItemCost  =(EditText)findViewById(R.id.itm_cost);
-        addItemprice =(EditText)findViewById(R.id.itm_price);
-        Save = (Button)findViewById(R.id.Save);
+        addItemCode = (EditText) findViewById(R.id.itm_code);
+        addItemName = (EditText) findViewById(R.id.itm_name);
+        addItemCost = (EditText) findViewById(R.id.itm_cost);
+        addItemprice = (EditText) findViewById(R.id.itm_price);
+        Save = (Button) findViewById(R.id.Save);
         Save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                String ItemCode=addItemCode.getText().toString();
-                String ItemName= addItemName.getText().toString();
-                String ItemCost=addItemCost.getText().toString();
+                String ItemCode = addItemCode.getText().toString();
+                String ItemName = addItemName.getText().toString();
+                String ItemCost = addItemCost.getText().toString();
                 String ItemPrice = addItemprice.getText().toString();
-                String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-
-                if (checkData(ItemCode,ItemName,ItemCost,ItemPrice)){
+                String date = new SimpleDateFormat("yyyy-MM-dd", Locale.US).format(new Date());
+                if (checkData(ItemCode, ItemName, ItemCost, ItemPrice)) {
                     DataManager dataManager = new DataManager(ItemsAddActivity.this);
-
-                    boolean result=  dataManager.addItem(Integer.parseInt(ItemCode),ItemName,Double.parseDouble(ItemCost),Double.parseDouble(ItemPrice),date);
-                    if ( result){
+                    boolean result = dataManager.addItem(Integer.parseInt(ItemCode), ItemName, Double.parseDouble(ItemCost), Double.parseDouble(ItemPrice), date);
+                    if (result) {
                         Toast.makeText(getBaseContext(), R.string.Done_Adding, Toast.LENGTH_LONG).show();
                         finish();
-                    }else {
+                    } else {
                         addItemCode.setError(getResources().getString(R.string.The_number_already_exists));
                         Toast.makeText(getBaseContext(), R.string.The_number_already_exists, Toast.LENGTH_LONG).show();
                     }
-
-
-
-
-
-                }else {
-
-
                 }
-
             }
         });
-
-        Cancel = (Button)findViewById(R.id.cancel);
+        Cancel = (Button) findViewById(R.id.cancel);
         Cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,18 +53,18 @@ Button Save,Cancel;
             }
         });
     }
-    public boolean checkData(String itm_code, String itm_name, String itm_cost,String itm_price) {
-        if ((itm_code != null && itm_name != null && itm_cost != null && itm_price !=null) &&
-                (itm_code.length() != 0 && itm_name.length() != 0 && itm_cost.length() != 0 && itm_price.length()!=0)) {
+
+    public boolean checkData(String itm_code, String itm_name, String itm_cost, String itm_price) {
+        if ((itm_code != null && itm_name != null && itm_cost != null && itm_price != null) &&
+                (itm_code.length() != 0 && itm_name.length() != 0 && itm_cost.length() != 0 && itm_price.length() != 0)) {
             return true;
         } else {
-            addItemCode.setError(getResources().getString(R.string.Pleaze_fill));
-            addItemName.setError(getResources().getString(R.string.Pleaze_fill));
-            addItemCost.setError(getResources().getString(R.string.Pleaze_fill));
-            addItemprice.setError(getResources().getString(R.string.Pleaze_fill));
-            Toast.makeText(getBaseContext(),  R.string.Pleaze_fill, Toast.LENGTH_LONG).show();
+            addItemCode.setError(getResources().getString(R.string.Please_fill));
+            addItemName.setError(getResources().getString(R.string.Please_fill));
+            addItemCost.setError(getResources().getString(R.string.Please_fill));
+            addItemprice.setError(getResources().getString(R.string.Please_fill));
+            Toast.makeText(getBaseContext(), R.string.Please_fill, Toast.LENGTH_LONG).show();
             return false;
         }
     }
-
 }
--- app/src/main/java/com/example/e3/mybills/ItemsEditActivity.java
+++ app/src/main/java/com/example/e3/mybills/ItemsEditActivity.java
@@ -8,57 +8,48 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class ItemsEditActivity extends AppCompatActivity {
-EditText code ,name,cost,price;
-Button save,cancel;
+    EditText code, name, cost, price;
+    Button save, cancel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items_edit);
 
-        code = (EditText)findViewById(R.id.Edit_itm_code);
+        code = (EditText) findViewById(R.id.Edit_itm_code);
         code.setFocusable(false);
         code.setEnabled(false);
-        //code.setCursorVisible(false);
-       // code.setKeyListener(null);
-        name = (EditText)findViewById(R.id.Edit_itm_name);
-        cost = (EditText)findViewById(R.id.Edit_itm_cost);
-        price = (EditText)findViewById(R.id.Edit_itm_price);
+        name = (EditText) findViewById(R.id.Edit_itm_name);
+        cost = (EditText) findViewById(R.id.Edit_itm_cost);
+        price = (EditText) findViewById(R.id.Edit_itm_price);
 
-        Bundle data =getIntent().getExtras();
-        int item_code=data.getInt("get_col_itm_code");
+        Bundle data = getIntent().getExtras();
+        int item_code = data.getInt("get_col_itm_code");
         String item_name = data.getString("get_col_itm_name");
         double item_cost = data.getDouble("get_col_itm_Cost");
         double item_price = data.getDouble("get_col_itm_price");
         final String item_date = data.getString("get_col_ad_date");
-
         code.setText(String.valueOf(item_code));
         name.setText(String.valueOf(item_name));
-        cost.setText(String.valueOf( item_cost));
-        price.setText(String.valueOf( item_price));
-
-
-        save=(Button)findViewById(R.id.Edit_Save);
+        cost.setText(String.valueOf(item_cost));
+        price.setText(String.valueOf(item_price));
+        save = (Button) findViewById(R.id.Edit_Save);
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                if (checkData(code.getText().toString(),name.getText().toString(),cost.getText().toString(),price.getText().toString())){
-                    final int itm_code=Integer.parseInt(code.getText().toString());
-                    final String itm_name =name.getText().toString();
+                if (checkData(code.getText().toString(), name.getText().toString(), cost.getText().toString(), price.getText().toString())) {
+                    final int itm_code = Integer.parseInt(code.getText().toString());
+                    final String itm_name = name.getText().toString();
                     final double itm_cost = Double.parseDouble(cost.getText().toString());
                     final double itm_price = Double.parseDouble(price.getText().toString());
-
                     DataManager update = new DataManager(ItemsEditActivity.this);
-                update.UpdateItem(itm_code,itm_name,itm_cost,itm_price,item_date);
-                Toast.makeText(getBaseContext(), R.string.Done_Edit, Toast.LENGTH_LONG).show();
-finish();
+                    update.UpdateItem(itm_code, itm_name, itm_cost, itm_price, item_date);
+                    Toast.makeText(getBaseContext(), R.string.Done_Edit, Toast.LENGTH_LONG).show();
+                    finish();
                 }
-
             }
         });
-
-
-        cancel = (Button)findViewById(R.id.Edit_cancel);
+        cancel = (Button) findViewById(R.id.Edit_cancel);
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,16 +57,17 @@ finish();
             }
         });
     }
-    public boolean checkData(String itm_code, String itm_name, String itm_cost,String itm_price) {
-        if ((itm_code != null && itm_name != null && itm_cost != null && itm_price !=null) &&
-                (itm_code.length() != 0 && itm_name.length() != 0 && itm_cost.length() != 0 && itm_price.length()!=0)) {
+
+    public boolean checkData(String itm_code, String itm_name, String itm_cost, String itm_price) {
+        if ((itm_code != null && itm_name != null && itm_cost != null && itm_price != null) &&
+                (itm_code.length() != 0 && itm_name.length() != 0 && itm_cost.length() != 0 && itm_price.length() != 0)) {
             return true;
         } else {
-            code.setError(getResources().getString(R.string.Pleaze_fill));
-            name.setError(getResources().getString(R.string.Pleaze_fill));
-            cost.setError(getResources().getString(R.string.Pleaze_fill));
-            price.setError(getResources().getString(R.string.Pleaze_fill));
-            Toast.makeText(getBaseContext(), R.string.Pleaze_fill, Toast.LENGTH_LONG).show();
+            code.setError(getResources().getString(R.string.Please_fill));
+            name.setError(getResources().getString(R.string.Please_fill));
+            cost.setError(getResources().getString(R.string.Please_fill));
+            price.setError(getResources().getString(R.string.Please_fill));
+            Toast.makeText(getBaseContext(), R.string.Please_fill, Toast.LENGTH_LONG).show();
             return false;
         }
     }
