9c98e4a9474a16949c439ead02633b3d084c463e
Ebrheem <broyemr@gmail.com>
2017-07-14 03:23:27 +0300

    اصلاح زر تغيير اللغة وتغيير الوان التطبيق

--- app/src/main/java/com/example/e3/mybills/BillActivity.java
+++ app/src/main/java/com/example/e3/mybills/BillActivity.java
@@ -1,16 +1,24 @@
 package com.example.e3.mybills;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class BillActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -75,6 +83,17 @@ public class BillActivity extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
+    public void setLocale(String lang) {
+        finish();
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, Start_screen.class);
+        startActivity(refresh);
+    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -94,6 +113,28 @@ public class BillActivity extends AppCompatActivity
             bundle.putInt("ImFromBillAdd",2);
             i.putExtras(bundle);
             startActivity(i);
+        }else if (id == R.id.Lang){
+            final CharSequence[] items = { "English", "العربية"};
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.Change_Language);
+            builder.setItems(items, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int pos) {
+                    if (pos == 0) {
+
+                        setLocale("en");
+                    } else if (pos == 1) {
+
+                        setLocale("ar");
+                    }
+
+                }
+            });
+
+            AlertDialog alert = builder.create();
+            alert.show();
+
+
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
