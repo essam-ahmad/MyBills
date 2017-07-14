4b2e20a7e08a683961c1e0e62f804f686fc0853c
Ebrheem <broyemr@gmail.com>
2017-07-14 03:36:25 +0300

    fix small bug

--- app/src/main/java/com/example/e3/mybills/ItemsAddEditActivity.java
+++ app/src/main/java/com/example/e3/mybills/ItemsAddEditActivity.java
@@ -51,6 +51,8 @@ public class ItemsAddEditActivity extends AppCompatActivity {
             _etItemName.setEnabled(false);
             layout=(android.support.design.widget.TextInputLayout)findViewById(R.id.itm_cost_input_layout);
             layout.setHint(getResources().getString(R.string.qty));
+
+            _etItemCost.setInputType(2);
             _etItemPrice.setText(String.valueOf(data.getDouble("get_col_itm_price")));
         }
         Save = (Button) findViewById(R.id.Save);
